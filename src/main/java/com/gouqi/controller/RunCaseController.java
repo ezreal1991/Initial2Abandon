b1d27820a9ebde449daf9d41fd8f52651ec22ca4
Ezreal1991 <957649465@qq.com>
2019-05-14 11:19:11 +0800

    增加get请求的发送功能

--- src/main/java/com/gouqi/controller/RunCaseController.java
+++ src/main/java/com/gouqi/controller/RunCaseController.java
@@ -1,8 +1,10 @@
 package com.gouqi.controller;
 
 import com.gouqi.entity.ParamBean;
+import com.gouqi.entity.TestCase;
 import com.gouqi.entity.UrlBean;
 import com.gouqi.service.RunCaseService;
+import com.gouqi.testcase.CollectionCase;
 import com.gouqi.util.UrlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -52,6 +55,51 @@ public class RunCaseController {
         for (UrlBean url : urlBeanList) {
             UrlUtil.getRealUrl(url);
         }
-        return null;
+        List<TestCase> resultList = new ArrayList<TestCase>();
+        CollectionCase.run("第一个testcase", resultList);
+        int errorNum = 0;
+        int failNum = 0;
+        int warningNum = 0;
+        int passNum = 0;
+        int ierrorNum = 0;
+        int ifailNum = 0;
+        int iwarningNum = 0;
+        int ipassNum = 0;
+        int iNum = 0;
+        for(TestCase testcase:resultList){
+            if(testcase.getErrorList().size()>0){
+                testcase.setCaseResult("error");
+                errorNum ++;
+            } else if(testcase.getFailList().size() > 0){
+                testcase.setCaseResult("fail");
+                failNum ++;
+            }	else if(testcase.getWarningList().size() > 0){
+                testcase.setCaseResult("warning");
+                warningNum ++;
+            } else{
+                testcase.setCaseResult("pass");
+                passNum ++;
+            }
+            ierrorNum += testcase.getErrorList().size();
+            ifailNum += testcase.getFailList().size();
+            iwarningNum += testcase.getWarningList().size();
+            ipassNum += testcase.getPassList().size();
+            iNum += testcase.getTotalList().size();
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("errorNum", errorNum);
+        modelAndView.addObject("failNum", failNum);
+        modelAndView.addObject("warningNum", warningNum);
+        modelAndView.addObject("passNum", passNum);
+        modelAndView.addObject("ierrorNum", ierrorNum);
+        modelAndView.addObject("ifailNum", ifailNum);
+        modelAndView.addObject("iwarningNum", iwarningNum);
+        modelAndView.addObject("ipassNum", ipassNum);
+        modelAndView.addObject("iNum", iNum);
+        modelAndView.addObject("caseNum", resultList.size());
+        modelAndView.addObject("resultList", resultList);
+        modelAndView.setViewName("/WEB-INF/jsp/report.jsp");
+        return modelAndView;
     }
 }
--- src/main/java/com/gouqi/util/UrlUtil.java
+++ src/main/java/com/gouqi/util/UrlUtil.java
@@ -1,5 +1,7 @@
 package com.gouqi.util;
 
+import com.gouqi.entity.InterfaceBean;
+import com.gouqi.entity.TestCase;
 import com.gouqi.entity.UrlBean;
 
 import java.util.ArrayList;
@@ -45,6 +47,46 @@ public class UrlUtil {
                 String paramStr = "${" + param + "}";
                 String url2 = url.getUrl().replace(paramStr, value);
                 url.setUrl(url2);
+            }
+        }
+    }
+
+    /**
+     * @Description // 将urlBean转化成interfaceBean
+     * @Date 10:35 2019/5/14
+     * @Param [urlBean]
+     * @return com.gouqi.entity.InterfaceBean
+     **/
+    public static InterfaceBean toInterface(UrlBean urlBean) {
+        InterfaceBean interfaceBean = new InterfaceBean();
+        interfaceBean.setName(urlBean.getName());
+        interfaceBean.setUrl(urlBean.getUrl());
+        interfaceBean.setExpectMs(urlBean.getExpectMs());
+        interfaceBean.setMethod(urlBean.getMethod());
+        return interfaceBean;
+    }
+
+    public static void setInfResult(TestCase testCase, InterfaceBean itf){
+        if (itf.getActualMs() == null) {
+            testCase.getErrorList().add(itf);
+            itf.setInterfaceResult("error");
+        } else {
+            if (itf.getJsonResult() != null) {
+                String code = itf.getJsonResult().getString("code");
+                // 断言
+                if (!"1".equals(code)) {
+                    testCase.getFailList().add(itf);
+                    itf.setInterfaceResult("fail");
+                } else {
+                    // 超时
+                    if (Long.parseLong(itf.getActualMs()) > Long.parseLong(itf.getExpectMs())) {
+                        testCase.getWarningList().add(itf);
+                        itf.setInterfaceResult("warning");
+                    } else {
+                        testCase.getPassList().add(itf);
+                        itf.setInterfaceResult("pass");
+                    }
+                }
             }
         }
     }
